f1d8d159c206ea5c27fa17afa3eefccc359691c3
leulephrem <leulbossy@gmail.com>
2022-10-15 21:38:26 +0300

    Update RegisterForm.cs
    validation

--- RegisterForm.cs
+++ RegisterForm.cs
@@ -52,11 +52,82 @@ namespace Stadium
             MemoryStream ms = new MemoryStream();
             pbox_user.Image.Save(ms, pbox_user.Image.RawFormat);
             byte[] photo = ms.ToArray();
-            User u = new User(txt_fname.Text, txt_lname.Text, txt_confirm.Text, txt_email.Text, txt_username.Text, txt_password.Text, photo);
-            u.createUser();
-            LoginForm l = new LoginForm();
-            l.Show();
-            this.Visible = false;
+            bool isVal = true;
+            //This regular expression refers to a pattern which accepts 5 to 15 characters with any lower case character, digit or special symbol “_-” only.
+            Regex username = new Regex(@"^[a-z0-9_-]{5,15}$");
+
+            //This regular expression refers to a pattern with at least one digit, one upper case letter, one lower case letter and one special symbol(“@#$%”).
+            Regex pass = new Regex(@"^((?=.*\d)(?=.*[A-Za - z])(?=.*[A - Z])(?=.*[@#$%]).{6,15})$");
+
+            //Email Format(name@example.com)
+            Regex email = new Regex(@"^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$");
+
+            if (!username.IsMatch(txt_username.Text.ToLower()))
+            {
+                errorProvider1.SetError(txt_username, "Invalid! 5 to 15 charachterz (allowed - and _");
+                isVal = false;
+
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+            if (!pass.IsMatch(txt_password.Text))
+            {
+                errorProvider4.SetError(txt_password, "Invalid! needed at least one digit, one upper case letter, one lower case letter and one special symbol(“@#$%”)");
+                isVal = false;
+
+            }
+            else
+            {
+                errorProvider4.Clear();
+            }
+            if (!email.IsMatch(txt_email.Text))
+            {
+                errorProvider1.SetError(txt_email, "Invalid! Format must be like name@example.com");
+                isVal = false;
+
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+            if (string.IsNullOrEmpty(txt_fname.Text))
+            {
+                errorProvider2.SetError(txt_fname, "Invalid! First Name cannot be empty");
+                isVal = false;
+            }
+            else
+            {
+                errorProvider2.Clear();
+            }
+            if (string.IsNullOrEmpty(txt_lname.Text))
+            {
+                errorProvider2.SetError(txt_lname, "Invalid! Last Name cannot be empty!");
+                isVal = false;
+            }
+            else
+            {
+                errorProvider2.Clear();
+            }
+            if (!(txt_password.Text == txt_confirm.Text))
+            {
+                errorProvider3.SetError(txt_confirm, "Invalid! write your password correctly");
+                isVal = false;
+            }
+            else
+            {
+                errorProvider3.Clear();
+            }
+
+            if (isVal)
+            {
+                User u = new User(txt_fname.Text, txt_lname.Text, txt_confirm.Text, txt_email.Text, txt_username.Text, txt_password.Text, photo);
+                u.createUser();
+                LoginForm l = new LoginForm();
+                l.Show();
+                this.Visible = false;
+            }
         }
 
         
